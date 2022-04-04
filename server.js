8247d0ef26ea699b4d78c647130ed4e9dad1f2c5
Rodney P <rodney.plancher@gmail.com>
2022-04-04 17:34:58 -0400

    added a get routs and working on post routes

--- server.js
+++ server.js
@@ -2,14 +2,37 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || "3001"
 
-const  notes = require('./db/db.json')
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+const notes = require('./db/db.json')
 
 
 app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
+function findById(id, notesArray) {
+  const result = notesArray.filter(notes => notes.id === id)
+  return result
+}
+
+app.get('/api/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes)
+  if (result) {
+    res.json(result)
+  } else {
+    res.send(404)
+  }
+}
+)
+
+app.post('/api/notes', (req, res) => {
+  console.log(req.body)
+  res.json(req.body)
+})
 
 app.listen(PORT, () => {
-  console.log("App is listening on Port: " + PORT);
+  console.log(`Server is up on port ${PORT}`);
 });
