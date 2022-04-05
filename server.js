56e1cab30e1f1c71c0106e33f383300c849a8df3
Rodney P <rodney.plancher@gmail.com>
2022-04-04 23:54:49 -0400

    finished addpost route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || "3001"
+const fs = require('fs')
+const path = require('path');
+// const { json } = require('stream/consumers');
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -8,16 +11,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const notes = require('./db/db.json')
 
-
+// api for getting all notes
 app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
+// function for finding one note by id
 function findById(id, notesArray) {
   const result = notesArray.filter(notes => notes.id === id)
   return result
 }
 
+// api for finding one note with id
 app.get('/api/notes/:id', (req, res) => {
   const result = findById(req.params.id, notes)
   if (result) {
@@ -25,14 +30,30 @@ app.get('/api/notes/:id', (req, res) => {
   } else {
     res.send(404)
   }
-}
-)
-
-app.post('/api/notes', (req, res) => {
-  console.log(req.body)
-  res.json(req.body)
 })
 
+// function for creating a new note
+function createNewNote(body, notesArray) {
+  const note = body
+  notesArray.push(note)
+  // writing to the db for notes
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify( notesArray, null, 2)
+    )
+    console.log(notesArray)
+  return note
+}
+
+
+// api for creating a note
+app.post('/api/notes', (req, res) => {
+  req.body.id = notes.length.toString();
+  const note = createNewNote(req.body, notes)
+  res.json(note)
+})
+
+// telling the app where to find my port.
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`);
 });
